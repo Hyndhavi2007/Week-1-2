942af05953f9b5bcffc723a6e6806c1144608452
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-02-20 22:30:44 +0530

    Add Level-01 Problem-01 Java code

---
+++ Age.java
@@ -0,0 +1,8 @@
+public class Age{
+    public static void main(String[] args) {
+        int currentYear = 2024;
+        int birtYear = 2000;
+        int age = currentYear - birtYear;
+        System.out.println("The age of the person is " + age);
+    }
+}
