1ee1bf6b4d0263eadb0b972a88c698ae85dd51a9
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-02-24 13:07:58 +0530

    Add Level-1 Problem-3 Java code

---
+++ DistanceCalculation.java
@@ -0,0 +1,9 @@
+public class DistanceCalculation {
+    public static void main(String[]args){
+        System.out.println("The given distance is in Kilometers");
+        float distance = 10.8f;
+        float mile = 1.6f;
+        float  distanceinMiles =  distance * mile;
+        System.out.println("The distance " + distance + " Kilometers in  Miles is " + distanceinMiles + "miles.");
+    }
+}
